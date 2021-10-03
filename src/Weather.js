b910752eda1a1816f69a4e00accf2324f2ffa7cf
Casto2710 <84015996+Casto2710@users.noreply.github.com>
2021-10-03 16:24:12 +0100

    fixed the error

--- src/Weather.js
+++ src/Weather.js
@@ -32,8 +32,7 @@ const[city,setCity]= useState(props.defaultcity);
   }
   function search() {
     const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
-    q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     
     axios.get(apiUrl).then(handleResponse);
   }
